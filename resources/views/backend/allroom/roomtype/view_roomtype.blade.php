3196e6e27375c680ea6bf5f207dba08aaa8e32b1
nhatvyx1010 <nguyenphamnhatvy1010@gmail.com>
2025-04-06 09:09:32 +0700

    dl-room-from-multi-table

--- resources/views/backend/allroom/roomtype/view_roomtype.blade.php
+++ resources/views/backend/allroom/roomtype/view_roomtype.blade.php
@@ -2,28 +2,25 @@
 @section('admin') 
 
 <div class="page-content"> 
-	<!--breadcrumb-->
-    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-         
-        <div class="ps-3">
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb mb-0 p-0">
-                    <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
-                    </li>
-                    <li class="breadcrumb-item active" aria-current="page">Room Type List</li>
-                </ol>
-            </nav>
-        </div>
-        <div class="ms-auto">
-            <div class="btn-group">
-                <a href="{{ route('add.room.type') }}" class="btn btn-primary px-5"> Add Room Type </a>
-                
-            </div>
-        </div>
-    </div>
-    <!--end breadcrumb-->
+    				<!--breadcrumb-->
+                    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+					<div class="ps-3">
+						<nav aria-label="breadcrumb">
+							<ol class="breadcrumb mb-0 p-0">
+								<li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+								</li>
+								<li class="breadcrumb-item active" aria-current="page">Room Type List</li>
+							</ol>
+						</nav>
+					</div>
+					<div class="ms-auto">
+						<div class="btn-group">
+                        <a href="{{ route('add.room.type') }}" class="btn btn-primary px-5"> Add Room Type </a>
+						</div>
+					</div>
+				</div>
+				<!--end breadcrumb-->
  
-     
     <hr/>
     <div class="card">
         <div class="card-body">
